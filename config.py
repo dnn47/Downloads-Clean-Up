62547a4086e09c08e1a2f6459f4839259c0b963f
Dune <dnn47@drexel.edu>
2023-12-21 13:18:31 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-emailSender = ""
-emailPassword = ""
-emailReceiver = ""
+emailSender = "YOUR_EMAIL_SENDER_ADDRESS"
+emailPassword = "YOUR_EMAIL_PASSWORD"
+emailReceiver = "YOUR_EMAIL_SECEIVER_ADDRESS"
